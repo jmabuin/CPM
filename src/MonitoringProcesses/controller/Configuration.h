2d47cd1f88e67c6f3c5e45bb56ec34f55fd82285
Jose Manuel Abuin Mosquera <abuinjm@gmail.com>
2016-02-16 15:42:39 +0100

    Configuration header comments updated

--- src/MonitoringProcesses/controller/Configuration.h
+++ src/MonitoringProcesses/controller/Configuration.h
@@ -22,52 +22,59 @@
 
 #include <QSettings>
 
+/*!
+ * \brief Struct that contains all parameters from configuration
+ */
 typedef struct Config{
-	std::string userName;
-	std::string password;
-	std::string nodes;
-	std::string nodesBM;
-	std::string port;
-	std::string key;
-	std::string processOwner;
-	std::string processName;
-	std::string processStartsWith;
-	std::string networkInterface;
-	bool checkMEM_Status;
-	bool checkCPU_Status;
-	bool checkPapi_Status;
+	std::string userName;		/*!< String containing the username */
+	std::string password;		/*!< String containing the password */
+	std::string nodes;		/*!< String containing the master nodes */
+	std::string nodesBM;		/*!< String containing the nodes behind master */
+	std::string port;		/*!< String containing the port */
+	std::string key;		/*!< String containing the key file */
+	std::string processOwner;	/*!< String containing the process owner */
+	std::string processName;	/*!< String containing the process name */
+	std::string processStartsWith;	/*!< String containing the start part of the process name */
+	std::string networkInterface;	/*!< String containing the network interface to use */
+	bool checkMEM_Status;		/*!< Boolean value used to measure or not the memory */
+	bool checkCPU_Status;		/*!< Boolean value used to measure or not the CPU percentage */
+	bool checkPapi_Status;		/*!< Boolean value used to measure or not PAPI counters */
 
 
 } Config;
 
+/*!
+ * \brief The Configuration class. Class that contains all the configuration parameters
+ */
+
 class Configuration
 {
 public:
-	Configuration();
-	Config getConfiguration();
-	void setConfiguration(Config conf);
+	Configuration();			/*!< Constructor */
+	Config getConfiguration();		/*!< Returns the Config struct filled with the current configuration parameters */
+	void setConfiguration(Config conf);	/*!< Sets the current configuration parameters */
 
 private:
-	QSettings *settings;
+	QSettings *settings;			/*!< QSettings pointer used to get and set the configuration parameters */
 
-	std::string configFile;
+	std::string configFile;			/*!< String containing the configuration file name */
 
-	std::string userKey;
-	std::string passwordKey;
-	std::string nodesKey;
-	std::string portKey;
-	std::string keyFileKey;
-	std::string nodesBMKey;
-	std::string networkKey;
-	std::string processOwnerKey;
-	std::string processNameKey;
-	std::string processStartsWithKey;
+	std::string userKey;			/*!< Key for the username */
+	std::string passwordKey;		/*!< Key for the password */
+	std::string nodesKey;			/*!< Key for nodes */
+	std::string portKey;			/*!< Key for port */
+	std::string keyFileKey;			/*!< Key for file containing public key */
+	std::string nodesBMKey;			/*!< Key for nodes behind master */
+	std::string networkKey;			/*!< Key for the network interface */
+	std::string processOwnerKey;		/*!< Key for the process owner */
+	std::string processNameKey;		/*!< Key for the process name */
+	std::string processStartsWithKey;	/*!< key for the string that contains the start of the process */
 
-	std::string measureCPU_Key;
-	std::string measureMEM_Key;
-	std::string measurePapi_Key;
+	std::string measureCPU_Key;		/*!< Key to measure or not CPU */
+	std::string measureMEM_Key;		/*!< Key to measure or not Memory */
+	std::string measurePapi_Key;		/*!< Key to measure or not PAPI counters */
 
-	std::string encryptDecrypt(std::string toEncrypt);
+	std::string encryptDecrypt(std::string toEncrypt);	/*!< Function to encrypt or decrypt a string */
 };
 
 #endif // CONFIGURATION_H
